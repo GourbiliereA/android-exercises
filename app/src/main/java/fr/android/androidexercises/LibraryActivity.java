259767252e4ee07dfd3e8a44308b9577e8f7b97f
Alex Gourbiliere <alex.gourbi@gmail.com>
2017-02-20 13:57:49 +0100

    Resolving Exercise 4

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -12,6 +12,10 @@ import android.widget.Toast;
 
 public class LibraryActivity extends AppCompatActivity {
 
+    public static int REQUEST_GET_BOOK = 1;
+
+    public static int RESULT_OK = 200;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,14 +29,25 @@ public class LibraryActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Replace with startActivityForResult() to start BookActivity
-                Toast.makeText(LibraryActivity.this, R.string.toast_todo, Toast.LENGTH_SHORT).show();
+                // Toast.makeText(LibraryActivity.this, R.string.toast_todo, Toast.LENGTH_SHORT).show();
+                startActivityForResult(new Intent(LibraryActivity.this, BookActivity.class), REQUEST_GET_BOOK);
             }
         });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // TODO get back book name
+        // Check which request we're responding to
+        if (requestCode == REQUEST_GET_BOOK) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                // The user picked a contact.
+                // The Intent's data Uri identifies which contact was selected.
+
+                // Do something with the contact here (bigger example below)
+                Toast.makeText(LibraryActivity.this, data.getStringExtra("bookName"), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
