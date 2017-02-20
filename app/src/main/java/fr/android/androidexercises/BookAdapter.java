f73fc7fd435d2ea83f4fadee2980e46a6715a952
Alex Gourbiliere <alex.gourbi@gmail.com>
2017-02-20 17:29:36 +0100

    Resolving Exercise 8

--- app/src/main/java/fr/android/androidexercises/BookAdapter.java
+++ app/src/main/java/fr/android/androidexercises/BookAdapter.java
@@ -1,6 +1,8 @@
 package fr.android.androidexercises;
 
 import android.content.Context;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -11,29 +13,41 @@ public class BookAdapter extends BaseAdapter {
 
     private final List<Book> books;
 
+    private final LayoutInflater layoutInflater;
+
     public BookAdapter(Context context, List<Book> books) {
         this.books = books;
-        // TODO LayoutInflater.from()
+        this.layoutInflater = LayoutInflater.from(context);
     }
 
     @Override
     public int getCount() {
         // TODO
+        return books.size();
     }
 
     @Override
     public Book getItem(int position) {
-        // TODO
+        return books.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        // TODO
+        return books.get(position).hashCode();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        // TODO
+        BookItemView view;
+        if (convertView == null) {
+            view = (BookItemView) layoutInflater.inflate(R.layout.custom_view_item_book, parent, false);
+        } else {
+            view = (BookItemView) convertView;
+        }
+
+        view.bindView(getItem(position));
+
+        return  view;
     }
 
 }
--- app/src/main/java/fr/android/androidexercises/BookItemView.java
+++ app/src/main/java/fr/android/androidexercises/BookItemView.java
@@ -2,9 +2,15 @@ package fr.android.androidexercises;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class BookItemView extends LinearLayout {
+
+    private TextView textView_name;
+    private TextView textView_price;
+    private ImageView imageView;
 
     public BookItemView(Context context) {
         this(context, null);
@@ -22,9 +28,15 @@ public class BookItemView extends LinearLayout {
     protected void onFinishInflate() {
         super.onFinishInflate();
         // TODO findViewById()
+        textView_name = (TextView) findViewById(R.id.nameTextView);
+        textView_price = (TextView) findViewById(R.id.priceTextView);
+        imageView = (ImageView) findViewById(R.id.imageView);
     }
 
     public void bindView(Book book) {
         // TODO setText()
+        textView_name.setText(book.name);
+        textView_price.setText(String.valueOf(book.price));
+        imageView.setImageResource(R.drawable.ic_launcher);
     }
 }
