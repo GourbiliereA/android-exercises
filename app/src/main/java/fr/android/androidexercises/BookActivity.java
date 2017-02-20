7687fef03f4745e66bfe3fb18e1ee53d45614722
Alex Gourbiliere <alex.gourbi@gmail.com>
2017-02-20 11:47:33 +0100

    Resolving Exercise 3

--- app/src/main/java/fr/android/androidexercises/BookActivity.java
+++ app/src/main/java/fr/android/androidexercises/BookActivity.java
@@ -1,7 +1,9 @@
 package fr.android.androidexercises;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class BookActivity extends AppCompatActivity {
 
