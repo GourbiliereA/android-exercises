0af76c9d0f0a438c77eb378f9490c107d7661a5c
Alex Gourbiliere <alex.gourbi@gmail.com>
2017-02-20 16:16:00 +0100

    Resolving Exercise 8

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -2,6 +2,10 @@ package fr.android.androidexercises;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.squareup.picasso.Picasso;
 
 public class LibraryActivity extends AppCompatActivity {
 
@@ -12,6 +16,9 @@ public class LibraryActivity extends AppCompatActivity {
 
         // TODO Get image view and load image form URL
         // http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg
+        ImageView image = (ImageView) findViewById(R.id.downloadImageView);
+        Picasso.with(this).load("http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg").into(image);
+//        Glide.with(this).load("http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg").centerCrop().into(image);
     }
 
 }
